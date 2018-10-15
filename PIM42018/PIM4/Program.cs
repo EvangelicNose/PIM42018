99fee455ecc65a759c10775643d78f5091dc41bd
samuel amorim <evangelicnose@gmail.com>
2018-10-15 15:33:29 -0300

    Alteração

--- PIM42018/PIM4/Program.cs
+++ PIM42018/PIM4/Program.cs
@@ -18,11 +18,9 @@ namespace PIM4
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
 
-                // O Form2 é o meu Login, aqui no exemplo, então vou exibi-lo
-                frmLogin f = new frmLogin();
-
-                // Se o OK for pressionado, eu abro o aplicativo, senão eu saio
-                if (f.ShowDialog() == DialogResult.OK)
+                frmLogin _frmLogin = new frmLogin();
+                Application.Run(_frmLogin);
+                if (_frmLogin.DialogResult == DialogResult.OK)
                     Application.Run(new frmMenu());
             }
         }
