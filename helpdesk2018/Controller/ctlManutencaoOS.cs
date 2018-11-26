63b731caefec80713fc2bc44d1b198c83aba1962
samuel amorim <evangelicnose@gmail.com>
2018-11-26 14:55:55 -0200

    Correcao ao listar Chamados

--- helpdesk2018/Controller/ctlManutencaoOS.cs
+++ helpdesk2018/Controller/ctlManutencaoOS.cs
@@ -16,15 +16,41 @@ namespace helpdesk2018.Controller
         {
             Conexao conexao = new Conexao();
             conexao.abrir();
-            string SQL = "SELECT tb_chamados.os AS OS, tb_empresas.nome AS EMPRESA, tb_motivos.descricao AS CAUSA," +
-                " tb_status.descricao AS STATUS," +
-                " tb_usuarios.usuario AS RESPONSÁVEL FROM (tb_empresas INNER JOIN tb_usuarios ON tb_empresas.idempresa = " +
-                "tb_usuarios.fk_idempresa) INNER JOIN(tb_status INNER JOIN" +
-                " (tb_motivos INNER JOIN tb_chamados ON tb_motivos.idmotivo =" +
-                " tb_chamados.fk_idmotivo) ON tb_status.idstatus = tb_chamados.fk_idstatus) " +
-                "ON tb_usuarios.idusuario = tb_chamados.fk_idusuario;";
+
+            string SQL;
+
+            if (mdlUsuario.Logado.Nivel == "0")
+            {
+                SQL = "SELECT tb_chamados.os AS OS, tb_empresas.nome AS EMPRESA, tb_motivos.descricao AS CAUSA," +
+                    " tb_status.descricao AS STATUS," +
+                    " tb_usuarios.usuario AS RESPONSÁVEL FROM (tb_empresas INNER JOIN tb_usuarios ON tb_empresas.idempresa = " +
+                    "tb_usuarios.fk_idempresa) INNER JOIN(tb_status INNER JOIN" +
+                    " (tb_motivos INNER JOIN tb_chamados ON tb_motivos.idmotivo =" +
+                    " tb_chamados.fk_idmotivo) ON tb_status.idstatus = tb_chamados.fk_idstatus) " +
+                    "ON tb_usuarios.idusuario = tb_chamados.fk_idusuario " +
+                    "WHERE fk_idusuario = @idusuario;";
+            }
+            else
+            {
+                SQL = "SELECT tb_chamados.os AS OS, tb_empresas.nome AS EMPRESA, tb_motivos.descricao AS CAUSA," +
+                    " tb_status.descricao AS STATUS," +
+                    " tb_usuarios.usuario AS RESPONSÁVEL FROM (tb_empresas INNER JOIN tb_usuarios ON tb_empresas.idempresa = " +
+                    "tb_usuarios.fk_idempresa) INNER JOIN(tb_status INNER JOIN" +
+                    " (tb_motivos INNER JOIN tb_chamados ON tb_motivos.idmotivo =" +
+                    " tb_chamados.fk_idmotivo) ON tb_status.idstatus = tb_chamados.fk_idstatus) " +
+                    "ON tb_usuarios.idusuario = tb_chamados.fk_idusuario;";
+            }
             OleDbCommand cmd = new OleDbCommand(SQL, conexao.GetConexao());
-            OleDbDataAdapter os = new OleDbDataAdapter(SQL, conexao.GetConexao());
+
+            var pmtIDUsuario = cmd.CreateParameter();
+            pmtIDUsuario.ParameterName = "@idusuario";
+            pmtIDUsuario.DbType = DbType.Int16;
+            pmtIDUsuario.Value = mdlUsuario.Logado.ID;
+            cmd.Parameters.Add(pmtIDUsuario);
+
+
+
+            OleDbDataAdapter os = new OleDbDataAdapter(cmd);
             DataTable ListaChamados = new DataTable();
             os.Fill(ListaChamados);
             return ListaChamados;
