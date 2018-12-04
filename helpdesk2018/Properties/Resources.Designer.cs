ccee5e7082e7d49ebdee4fb92e860affc02a78a4
NicolaAlmeida <nicolaweb2005@gmail.com>
2018-12-03 22:42:12 -0200

    Fix Resources files...

--- helpdesk2018/Properties/Resources.Designer.cs
+++ helpdesk2018/Properties/Resources.Designer.cs
@@ -99,25 +99,5 @@ namespace helpdesk2018.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Frm_Sobre_PIM1 {
-            get {
-                object obj = ResourceManager.GetObject("Frm_Sobre_PIM1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Frm_Sobre_PIM2 {
-            get {
-                object obj = ResourceManager.GetObject("Frm_Sobre_PIM2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
